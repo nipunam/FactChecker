91521dcc7006712a6b4502d75fe9029adaa4c755
Satya Madala <samadala@microsoft.com>
2019-07-22 18:06:46 +0530

    Get sentence from wikipage

--- FactChecker/FactChecker/FactChecker.py
+++ FactChecker/FactChecker/FactChecker.py
@@ -1,1 +1,10 @@
+import wikipedia
 
+print("Hello")
+print(wikipedia.summary("Andhra Pradesh"))
+p = wikipedia.page("Andhra Pradesh")
+print("Content =====")
+print(p.content);
+print("Sections =====")
+print(p.sections);
+print("End")
